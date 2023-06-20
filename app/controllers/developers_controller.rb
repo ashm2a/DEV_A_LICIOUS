b65bc6dc0703c769f4b76386796894c2cd67b8c5
audreysantoni <santoni.audrey@gmail.com>
2023-06-20 11:29:44 +0200

    pundit installé sans admin

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -2,29 +2,38 @@ class DevelopersController < ApplicationController
   before_action :set_developer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @developers = Developer.all
+    @developers = policy_scope(Developer)
   end
 
-  def show; end
+  def show
+    authorize @developper
+  end
 
   def new
     @developer = Developer.new
+    authorize @developer
   end
 
   def create
+    authorize @developer
     @developer = Developer.new(developer_params)
+    @developer.user = current_user
     @developer.save
     redirect_to developer_path(@developer)
   end
 
-  def edit; end
+  def edit
+    authorize @developer
+   end
 
   def update
+    authorize @developer
     @developer.update!(developer_params)
     redirect_to developer_path(@developer)
   end
 
   def destroy
+    authorize @developer
     @developer.destroy
     redirect_to developers_path, status: :see_other
   end
