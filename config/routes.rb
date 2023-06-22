af4b954a97af91515e0abbec367b04fa6f8ea98e
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-06-22 11:39:33 +0200

    developers list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "developers#index"
+
+  devise_for :users
+  get 'profile', to: 'pages#profile', as: 'user_profile'
+
   resources :developers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
